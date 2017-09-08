e0af9d2bc2dbcd536b11a69ab57bc656b3e1b1e4
晏立 <yanli@yellfun.com>
2017-09-08 11:17:45 +0800

    调试完毕，可运行

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,135 +30,17 @@ Page({
   },
 
   onLoad: function (options) {
-
-    function onGetSystemInfo() {
-      return new Promise(function(resolve, reject){
-        wx.getSystemInfo({
-          success: function (res) { resolve(res) },
-          fail: function (res) { reject(res) }
-        })
-      })
-    }
-
-    var self = this
-
-    var deviceId = 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825'
-
-    var serverId = null
-
-    function onOpenBluetoothAdapter() {
-
-      return new Promise(function (resolve, reject) {
-        wx.openBluetoothAdapter({
-          success: function (res) { resolve() },
-          fail: function (res) { reject(res) }
-        })
-      })
-    }
-    function onCreateConnection() {
-      return new Promise(function (resolve, reject) {
-        wx.createBLEConnection({
-          deviceId:deviceId,
-          success: function (res) { resolve() },
-          fail: function (res) { reject(res)}
-        })
-      })
-    }
-    function onGetDeviceServices() {
-      return new Promise(function (resolve, reject) {
-        wx.getBLEDeviceServices({
-          deviceId: deviceId,
-          success: function (res) { resolve(res) },
-          fail: function (res) { reject(res) }
-        })
-      })
-    }
-    function onGetBLEDeviceCharacteristics(serverid) {
-      return new Promise(function (resolve, reject) {
-        wx.getBLEDeviceCharacteristics({
-          deviceId: deviceId,
-          serviceId: serverId,
-          success: function (res) { resolve(res) },
-          fail: function (res) { reject(res) }
-        })
-      })
-    }
-    function onResult(res) {
-
-      for (var i = 0; i < res.characteristics.length; ++i) {
-
-        if (res.characteristics[i].properties.indicate) {
-
-          app.globalData.indicatecharacteristicId = res.characteristics[i].uuid
-
-          break
-        }
-      }
-
-      for (var i = 0; i < res.characteristics.length; ++i) {
-
-        if (res.characteristics[i].properties.write) {
-
-          app.globalData.writecharacteristicId = res.characteristics[i].uuid
-
-          break
-        }
-      }
-
-      self.onConnectSuccess()
-    }
-
-    onGetSystemInfo()
-    .then(function(res){
-
-      app.globalData.ios = (res.model.indexOf('iPhone') >= 0)
-
-      return onOpenBluetoothAdapter()   
-    })
-    .then(function(){
-
-      return onCreateConnection()
-      
-    })
-    .catch(function(error){
-
-      throw error
-    })
-    .then(function(){
-
-      return onGetDeviceServices()
-
-    })
-    .catch(function (error) {
-
-      throw error
-      
-    })
-    .then(function(res){
-
-      serverId = res.services[0].uuid
-
-      return onGetBLEDeviceCharacteristics(serverId)
-
-    })
-    .catch(function(error){
-
-      throw error
-    })
-    .then(function(res){
-
-      onResult(res)
-    })
-    .catch(function(error){
-
-      // self.onConnectFailed()
-      setTimeout(function(){
-
-        self.onConnectSuccess()
-
-      }, 500)
-    
-      console.log(error)
-    })
+  	
+  	app.doConnect((result) => {
+	
+			if (result) {
+				
+				this.onConnectSuccess()
+			}
+			else {
+				
+				this.onConnectSuccess()
+			}
+		})
   },
 })
--- pages/main/main.js
+++ pages/main/main.js
@@ -1,5 +1,14 @@
 // main.js
 var app = getApp()
+
+var doorStatus = {
+		payfailed: 0,
+		unlockfailed: 1,
+		unlocksuccess: 2,
+		unlocking: 3,
+		unpay: 4
+}
+
 Page({
 
   /**
@@ -7,16 +16,13 @@ Page({
    */
   data: {
     ticket:{
-      payfailed: 0,
-      unlockfailed: 1,
-      unlocksuccess: 2,
-      unlocking: 3,
-      unpay: 4,
       doorStatus:4,
       message: '全程统一票价', 
     },
     bottom: {
+    	needReunlock:0,
       paysuccess:0,
+			title:'立即支付'
     },
     card: {
       userHeadicon:'http://wx.indoorun.com/indoorun/indoorun/unlock/dist/static/avatar.png'
@@ -36,21 +42,65 @@ Page({
     })
   },
 
-  onPaySuccess:function(){
+  promisePayOrder:function(){
 
-
+		return new Promise(function(resolve, reject) {
+			
+			setTimeout(function() {
+				
+				resolve(true)
+				
+			}, 1000)
+		})
   },
 
-  payorder: function () {
+  unlock: function () {
   
-    this.setData({
-      'bottom.paysuccess':1
-    })
-
-    this.setData({
-      'ticket.doorStatus': 3,
-      'ticket.message' : '开闸中...!'
-    })
+  	this.promisePayOrder()
+			.then(()=>{
+				
+				this.setData({
+					'bottom.paysuccess':1,
+					'ticket.doorStatus': doorStatus.unlocking,
+					'ticket.message' : '开闸中...!'
+				})
+				
+				return Promise.resolve()
+			})
+			.catch(()=> {
+				
+				this.setData({
+					'bottom.paysuccess':0,
+					'ticket.doorStatus': doorStatus.payfailed,
+					'ticket.message' : '支付失败，请重新支付!'
+				})
+				
+				return Promise.reject()
+			})
+			.then(()=> {
+				
+				app.unLock((result) => {
+					
+					if (result) {
+						
+						this.setData({
+							'ticket.doorStatus': doorStatus.unlocksuccess,
+							'ticket.message' : '闸机开启成功，祝您乘车愉快!'
+						})
+					}
+					else {
+						
+						this.setData({
+							'ticket.doorStatus': doorStatus.unlockfailed,
+							'ticket.message' : '闸机开启失败，请重试!',
+							'bottom.title' : '重新开闸'
+						})
+					}
+				})
+			})
+			.catch(()=>{
+  	
+			})
   },
 
   /**
